bbf3b5805291d59bc6d42aa18057b57b7e0f811c
Camilo Orrego <camilo.orrego@satrack.com>
2019-07-18 19:13:07 -0500

    Wish complete

--- app/views/wishlist/wishlist.component.js
+++ app/views/wishlist/wishlist.component.js
@@ -5,33 +5,37 @@ angular.
   component('wishlistPage', {
     templateUrl: '/views/wishlist/wishlist.template.html',
     controller:
-      ($scope) => {
+      ($scope, $window) => {
 
-        let imagePath = '';
-        $scope.todos = [
-          {
-            face: '/img/television.png',
-            what: 'Television',
-            who: 'My location',
-            when: '3:08PM',
-            notes: " I would like a television"
-          },
-          {
-            face: '/img/iphone.png',
-            what: 'Brunch this weekend?',
-            who: 'Min Li Chan',
-            when: '3:08PM',
-            notes: " I'll be in your neighborhood doing errands"
-          },
-          {
-            face: imagePath,
-            what: 'Brunch this weekend?',
-            who: 'Min Li Chan',
-            when: '3:08PM',
-            notes: " I'll be in your neighborhood doing errands"
-          }
-        ];
+        $scope.todos = [];
 
+        $scope.login = JSON.parse($window.sessionStorage.getItem('login'));
+
+        if ($scope.login.user) {
+          $scope.todos = [
+            {
+              face: '/img/television.png',
+              what: 'Television',
+              who: 'My location',
+              when: '3:08PM',
+              notes: "I would like a television"
+            },
+            {
+              face: '/img/iphone.png',
+              what: 'Mobile',
+              who: 'My city',
+              when: '3:08PM',
+              notes: "I would like a mobile"
+            },
+            {
+              face: '/img/pc.png',
+              what: 'Laptop',
+              who: 'My apartment',
+              when: '3:08PM',
+              notes: "I would like a laptop"
+            }
+          ];
+        }
       }
 
   });
